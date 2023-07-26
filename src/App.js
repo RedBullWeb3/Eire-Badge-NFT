3f07787c8538173add45eac9f67130de29f28893
RedBull3 <65456462+RedBullWeb3@users.noreply.github.com>
2023-07-27 00:20:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ export const StyledButton = styled.button`
   padding: 5px;
   border-radius: 5px;
   border: none;
-  background-color: none;
+  background-color: var(--accent);
   padding: 10px;
   font-weight: bold;
   color: var(--secondary-text);
-  width: 70px;
+  width: 100px;
   cursor: pointer;
   box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
   -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
@@ -32,7 +32,7 @@ export const StyledRoundButton = styled.button`
   padding: 10px;
   border-radius: 100%;
   border: none;
-  background-color: none;
+  background-color: var(--accent);
   padding: 10px;
   font-weight: bold;
   font-size: 15px;
